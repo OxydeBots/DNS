0e45a7c45848d23eeb457fbe73a2f8d28a064bc2
Oxyde <157843247+OxydeDev1789@users.noreply.github.com>
2024-12-05 22:37:17 +0100

    Mise à jour du fichier

--- dnsfinder.js
+++ dnsfinder.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const dns =require('dns');
-const d= process.env.DOMAIN;
+const dns = require('dns');
+const d = process.env.DOMAIN;
 const f = () => {
         let t = new Date().toLocaleTimeString()
 
         dns.resolve4(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
         dns.resolve6(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
     }
-setInterval(f,5000);
+setInterval(f,5000); //5000 = 5 seconde - 10000 = 10 secondes
