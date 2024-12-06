dc94e375b2594d73519d5e3eb4fadecd6b73cf38
Oxyde <157843247+OxydeDev1789@users.noreply.github.com>
2024-12-06 22:00:25 +0100

    Update dnsfinder.js
    Commentaire à jour

--- dnsfinder.js
+++ dnsfinder.js
@@ -7,4 +7,4 @@ const f = () => {
         dns.resolve4(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
         dns.resolve6(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
     }
-setInterval(f,5000); //5000 = 5 seconde - 10000 = 10 secondes
+setInterval(f,5000); //5000 = 5 seconde | 10000 = 10 secondes
