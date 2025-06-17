1697c0a9f25d179f236ff8d0b7db787980e0898e
Oxyde Bot's <gabsamw@oxyde-bots.xyz>
2025-06-17 21:41:46 +0200

    Update dnsfinder.js

--- dnsfinder.js
+++ dnsfinder.js
@@ -3,7 +3,6 @@ const dns = require('dns');
 const d = process.env.DOMAIN;
 const f = () => {
         let t = new Date().toLocaleTimeString()
-
         dns.resolve4(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
         dns.resolve6(d,(e,a)=>{console.log(`[${t}] ${d} -> ${!e&&a?a.join(', '):'Aucune IP'}`);});
     }
